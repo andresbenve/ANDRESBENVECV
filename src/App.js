7a5ba71e3e1500c713f5e8a76f7d3d3c4eea881f
ANDRÉS BENVENUTO <56841920+andresbenve@users.noreply.github.com>
2022-03-08 19:04:01 -0300

    Casi terminado Falta alinear

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Screen from "./Screen.png";
+import ScreenSpanish from "./ScreenSpanish.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={Screen} alt="screen" height={400} className={App} />
+        <img src={ScreenSpanish} alt="screen" height={400} className={App} />
       </header>
     </div>
   );
